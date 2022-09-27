0107fc87108049003d930f1767c4ab8cba82d870
Pinxi Tan <tpinxi@gmail.com>
2022-09-27 16:13:59 +0800

    feat(data-gen): Change to static method

--- data_generation/models.py
+++ data_generation/models.py
@@ -9,17 +9,22 @@ from data_generation.random_util import get_random_time, get_random_room
 class BasePoint(BaseModel):
     is_actionable: bool
 
+    @staticmethod
     @abstractmethod
-    def get_point(self) -> str:
+    def get_point() -> str:
         ...
 
 
 class BookMeetingPoint(BasePoint):
     is_actionable: bool = True
 
-    def get_point(self) -> str:
+    @staticmethod
+    def get_point() -> str:
         action = random.choice(["Schedule", "Book", "Plan"])
         location = random.choice([f" in {get_random_room()}", ""])
         return random.choice([
             f"{action} a meeting{location} at {get_random_time()}",
         ])
+
+if __name__ == "__main__":
+    print(BookMeetingPoint.get_point())
