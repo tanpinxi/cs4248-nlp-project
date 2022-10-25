cf680aa453f31b5e92cff578d4a229debe14f06a
Pinxi Tan <tpinxi@gmail.com>
2022-10-26 00:00:59 +0800

    fix(evaluation): Add gpt3 baseline evaluation

--- evaluation/gpt_logprob.py
+++ evaluation/gpt_logprob.py
@@ -1,4 +1,5 @@
 import json
+import random
 from typing import List, Union, Tuple
 
 import openai
@@ -11,6 +12,8 @@ def get_gpt_logprobs(input: str, model: str) -> Tuple[float, int]:
         prompt=input,
         max_tokens=0,
         temperature=0,
+        logprobs=0,
+        echo=True,
         stop=[""],
     )
     res_dict = res["choices"][0]
@@ -24,37 +27,52 @@ def get_gpt_logprobs(input: str, model: str) -> Tuple[float, int]:
     completion_tokens = 0
     completion_logprob = 0.0
     for i in range(len(tokens) - 1, -1, -1):
-        if tokens[i] != "END" and offsets[i] >= prompt_offset:
+        if tokens[i] == "===":
+            break
+        elif tokens[i] != "END":
             completion_tokens += 1
             completion_logprob += logprobs[i]
-        elif offsets[i] < prompt_offset:
-            break
     return completion_logprob, completion_tokens
 
-def calc_gpt_logprobs(data: List[str], model: str) -> float:
+def calc_gpt_logprobs(data: List[str], model: str, num_shots: int) -> float:
     running_average_logprob = 0.0
-    for x in data:
-        logprob, tokens = get_gpt_logprobs(x, model)
+    for i, x in enumerate(data):
+        if num_shots == 0:
+            text = x
+        else:
+            if i < len(data) / 2:
+                primers = random.sample(data[i+1:], num_shots)
+            else:
+                primers = random.sample(data[:i], num_shots)
+            primers.append(x)
+            text = "\n\n".join(primers)
+        logprob, tokens = get_gpt_logprobs(text, model)
         running_average_logprob += logprob / tokens
     return running_average_logprob / len(data)
 
-def create_prompt(email: str, summary: str) -> str:
+def create_text(email: str, summary: str) -> str:
     return email.strip() + "\n\n===\n\n" + summary.strip() + "\nEND"
 
 if __name__ == "__main__":
     openai.api_key = OPENAI_API_KEY
-    model_names = [
-        "ada:ft-personal:cs4248-2022-10-15-05-02-44",
-        "babbage:ft-personal:cs4248-2022-10-15-06-31-30",
-        "curie:ft-personal:cs4248-2022-10-15-07-01-21",
+    models = [
+        ("ada", 4),
+        ("text-ada-001", 0),
+        ("ada:ft-personal:cs4248-2022-10-15-05-02-44", 0),
+        ("babbage", 4),
+        ("text-babbage-001", 0),
+        ("babbage:ft-personal:cs4248-2022-10-15-06-31-30", 0),
+        ("curie", 4),
+        ("text-curie-001", 0),
+        ("curie:ft-personal:cs4248-2022-10-15-07-01-21", 0),
     ]
 
-    with open("data/handwritten_data.jsonl", "r") as f:
+    with open("data/mingann.jsonl", "r") as f:
         json_data = list(map(json.loads, f))
-    data = [create_prompt(email=x["email"], summary=x["summary"]) for x in json_data]
+    data = [create_text(x["email"], x["summary"]) for x in json_data]
 
-    for model in model_names:
-        logprob = calc_gpt_logprobs(data=data, model=model)
-        print("========")
-        print(model)
+    for m in models:
+        model_name, num_shots = m
+        logprob = calc_gpt_logprobs(data=data, model=model_name, num_shots=num_shots)
+        print(model_name, num_shots)
         print("Average Log Prob:", round(logprob, 6))
