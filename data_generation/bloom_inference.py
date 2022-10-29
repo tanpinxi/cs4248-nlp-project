e7346a5a4b10e9f70347ffa102942d2d3cab7cfd
Pinxi Tan <tpinxi@gmail.com>
2022-10-29 12:13:25 +0800

    feat(data-gen): Add bloom generated data

--- data_generation/bloom_inference.py
+++ data_generation/bloom_inference.py
@@ -1,24 +1,93 @@
+import time
+
 from huggingface_hub.inference_api import InferenceApi
 
 from settings import HUGGINGFACE_API_KEY
 
 
-def get_bloom_output(prompt: str) -> str:
+def get_bloom_output(prompt: str, end_char: str = "===") -> str:
     inference = InferenceApi(repo_id="bigscience/bloom", token=HUGGINGFACE_API_KEY)
-    res = inference(prompt, params={"max_new_tokens": 150, "temperature": 0.6, "return_full_text": False})
-    return res[0]["generated_text"].strip()
+    res = inference(
+        prompt,
+        params={"max_new_tokens": 150, "temperature": 0.6, "return_full_text": False},
+    )
+    try:
+        output = res[0]["generated_text"].strip()
+    except KeyError:
+        print("Error from Bloom")
+        print(res)
+        print("Retrying in 1 min...")
+        time.sleep(62)
+        return get_bloom_output(prompt=prompt, end_char=end_char)
+    output = output[len(prompt) :]
+    end_idx = output.find(end_char)
+    return output[:end_idx].strip()
 
 
 if __name__ == "__main__":
     prompt = """Write an email elaborating on all of the following points. 
+
+===
+
+Sender: Roberto
+Receiver: Lee, a colleague at work
+- ask him to explain the industry news again
+- say that you look forward to working with him in the future
+- say you had a hard time on call with him
+- say you miss the email from him
+- scold him for missing the meeting
+Rephrase the above points to sound natural. To not list them in point form. Write the email in a friendly and casual tone.
+
+Email:
+
+Hi Lee,
+
+I hope you're doing well. I was wondering if you could explain the industry news to me again. I'm really looking forward to working with you in the future.
+
+I had a hard time on call with you the other day. I miss getting your emails. Please try to be more responsive in the future.
+
+Take care,
+
+Roberto
+
+===
+
+Write an email elaborating on all of the following points. 
+Sender: Sara
+Receiver: Janet, a colleague at work
+- tell her there is no need for email
+- ask her to confirm that tomorrow's meeting agenda is about annual budget
+- ask her to endorse Trisha's request to go on childcare leave
+- ask her to clarify the topic again
+- scold her for giving you a hard time
+Rephrase the above points to sound natural. To not list them in point form. Write the email in a rude and annoyed tone.
+
+Email:
+
+Hey Janet,
+
+I'm getting a little sick of all these emails. Can we just rely on face-to-face communication for once?
+
+Also, can you confirm that tomorrow's meeting is going to be about the annual budget? I don't want to waste my time if it's not.
+
+And while we're at it, can you endorse Trisha's request for childcare leave? I don't see why you're giving her a hard time.
+
+Clarify the topic again? Are you kidding me? I think you need to stop emailing me and actually talk to me in person.
+
+Seriously, Janet. This is getting ridiculous. Stop emailing me and talk to me face-to-face.
+
+Sara
+
+===
+
 Sender: Paul, the Head of IT
 Receiver: John, a colleague at work
 - thank him for the call this afternoon
 - ask him to review the attached legal contract by tomorrow
 - say you are more familiar with the restructuring planned for next Quarter
 - ask him to book a meeting at 4pm next Wednesday
-Rephrase the above points to sound natural. To not list them in point form.
-===
+Rephrase the above points to sound natural. To not list them in point form. Write the email in a friendly tone.
+
 Email:
 """
     output = get_bloom_output(prompt)
--- data_generation/data_generator.py
+++ data_generation/data_generator.py
@@ -2,13 +2,16 @@ import json
 import random
 from typing import List, Callable, Type, Tuple, Any
 
+from jsonlines import jsonlines
+
 from data_generation.actionable_models import *
 from data_generation.bloom_inference import get_bloom_output
 from data_generation.gpt_inference import get_gpt_output
 from data_generation.nonactionable_models import *
 from data_generation.util_models import ActionablePoint, NonActionablePoint, BasePoint
 
-data_file = "../data/lm_generated_data.jsonl"
+data_output_file = "../data/bloom_generated_data.jsonl"
+data_input_file = "../data/gpt_generated_data.jsonl"
 
 all_actionable_points: List[Type[ActionablePoint]] = [
     BookMeetingPoint,
@@ -23,7 +26,7 @@ all_actionable_points: List[Type[ActionablePoint]] = [
     RequestUpdatePoint,
     RequestResponsePoint,
     FeedbackPoint,
-    ClarificationPoint
+    ClarificationPoint,
 ]
 
 all_nonactionable_points: List[Type[NonActionablePoint]] = [
@@ -42,14 +45,16 @@ all_nonactionable_points: List[Type[NonActionablePoint]] = [
     NegativeMailPoint,
     SchoolPoint,
     ThankPoint,
-    ApologyPoint       
+    ApologyPoint,
 ]
 
+
 def generate_prompt_and_summary(
-        actionable_points: List[ActionablePoint],
-        nonactionable_points: List[NonActionablePoint],
-        sender_is_male: bool,
-        recipient_is_male: bool
+    priming_examples: List[str],
+    actionable_points: List[ActionablePoint],
+    nonactionable_points: List[NonActionablePoint],
+    sender_is_male: bool,
+    recipient_is_male: bool,
 ) -> Tuple[str, str]:
     recipient_pronoun = "him" if recipient_is_male else "her"
     points: List[BasePoint] = []
@@ -63,9 +68,18 @@ def generate_prompt_and_summary(
         for x in points
     ]
     points_block = "\n".join(point_strings)
-    tone = random.choice(["friendly and casual", "formal and respectful", "excited and energetic", "rude and annoyed"])
-    prompt = f"""Write an email elaborating on all of the following points. 
-Sender: {get_random_first_name(sender_is_male)}
+    tone = random.choice(
+        [
+            "friendly and casual",
+            "formal and respectful",
+            "excited and energetic",
+            "rude and annoyed",
+        ]
+    )
+    few_shot_primer = (
+        "\n\n===\n\n".join([""] + priming_examples + [""]) if priming_examples else "\n"
+    )
+    prompt = f"""Write an email elaborating on all of the following points. {few_shot_primer}Sender: {get_random_first_name(sender_is_male)}
 Receiver: {get_random_first_name(recipient_is_male)}, a colleague at work
 {points_block}
 Rephrase the above points to sound natural. To not list them in point form. Write the email in a {tone} tone.
@@ -73,22 +87,40 @@ Rephrase the above points to sound natural. To not list them in point form. Writ
 Email:
 """
     summarized_points = (
-        "\n".join(["- " + x.summarized_point for x in points if isinstance(x, ActionablePoint)])
+        "\n".join(
+            [
+                "- " + x.summarized_point
+                for x in points
+                if isinstance(x, ActionablePoint)
+            ]
+        )
         if len(actionable_points) > 0
         else "NONE"
     )
     return prompt, summarized_points
 
-def create_email_data(gen_func: Callable[[str], str]) -> Tuple[str, str, str]:
+
+def create_email_data(
+    priming_examples: List[str], gen_func: Callable[[str], str]
+) -> Tuple[str, str, str]:
     sender_is_male = bool(random.getrandbits(1))
     recipient_is_male = bool(random.getrandbits(1))
     n_actionable = random.randint(0, 3)
     n_nonactionable = random.randint(2, 5)
-    actionable_classes = random.sample(all_actionable_points, k=n_actionable) if n_actionable > 0 else []
-    nonactionable_classes = random.sample(all_nonactionable_points, k=n_nonactionable) if n_nonactionable > 0 else []
+    actionable_classes = (
+        random.sample(all_actionable_points, k=n_actionable) if n_actionable > 0 else []
+    )
+    nonactionable_classes = (
+        random.sample(all_nonactionable_points, k=n_nonactionable)
+        if n_nonactionable > 0
+        else []
+    )
     actionable_points = [x.init_point(recipient_is_male) for x in actionable_classes]
-    nonactionable_points = [x.init_point(recipient_is_male) for x in nonactionable_classes]
+    nonactionable_points = [
+        x.init_point(recipient_is_male) for x in nonactionable_classes
+    ]
     email_prompt, summarized_points = generate_prompt_and_summary(
+        priming_examples=priming_examples,
         actionable_points=actionable_points,
         nonactionable_points=nonactionable_points,
         sender_is_male=sender_is_male,
@@ -96,6 +128,7 @@ def create_email_data(gen_func: Callable[[str], str]) -> Tuple[str, str, str]:
     )
     email = gen_func(email_prompt)
     return email_prompt, email, summarized_points
+
 
 def save_data(f_out, email_prompt: str, email: str, summarized_points: str):
     data = {
@@ -106,14 +139,40 @@ def save_data(f_out, email_prompt: str, email: str, summarized_points: str):
     json.dump(data, f_out)
     f_out.write("\n")
 
-def run(n: int, gen_func: Callable[[str], str]):
-    f_out = open(data_file, "a+")
-    for i in range(n):
+
+def get_example_data(max_examples: int = 10) -> List[str]:
+    examples: List[str] = []
+    with jsonlines.open(data_input_file, "r") as reader:
+        for item in reader:
+            prompt = item["email_prompt"].strip().split("\n", 1)[1]
+            completion = item["email"].strip()
+            example = prompt + completion
+            examples.append(example)
+            if len(examples) >= max_examples:
+                break
+    print(f"Found {len(examples)} few-shot priming examples points.")
+    return examples
+
+
+def run(num_data: int, num_shots: int, gen_func: Callable[[str], str]):
+    all_examples: List[str] = get_example_data() if num_shots > 0 else []
+    f_out = open(data_output_file, "a+")
+    for i in range(num_data):
         if i % 10 == 0:
             print(i)
-        email_prompt, email, summarized_points = create_email_data(gen_func=gen_func)
-        save_data(f_out=f_out, email_prompt=email_prompt, email=email, summarized_points=summarized_points)
+        examples = random.sample(all_examples, num_shots)
+        random.shuffle(examples)
+        email_prompt, email, summarized_points = create_email_data(
+            priming_examples=examples, gen_func=gen_func
+        )
+        save_data(
+            f_out=f_out,
+            email_prompt=email_prompt,
+            email=email,
+            summarized_points=summarized_points,
+        )
     f_out.close()
+
 
 if __name__ == "__main__":
     while True:
@@ -123,7 +182,15 @@ if __name__ == "__main__":
         except ValueError:
             print("Enter a valid number.")
     while True:
-        gen_method = input("Which LM to generate data with? [bloom/gpt]: ").lower().strip()
+        try:
+            num_shots = int(input("How many few-shot examples to use: "))
+            break
+        except ValueError:
+            print("Enter a valid number.")
+    while True:
+        gen_method = (
+            input("Which LM to generate data with? [bloom/gpt]: ").lower().strip()
+        )
         if gen_method == "bloom":
             gen_func = get_bloom_output
             break
@@ -133,4 +200,4 @@ if __name__ == "__main__":
         else:
             print("Enter a valid method.")
     print("Generating data...")
-    run(n=num_data, gen_func=gen_func)
+    run(num_data=num_data, num_shots=num_shots, gen_func=gen_func)
