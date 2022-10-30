67090cbe28cfa9de3c04bdc3c0b5ce850ee2753d
patricktan6 <tanxizhe@gmail.com>
2022-10-30 16:19:04 +0800

    Added handwritten datapoints

--- data/handwritten_helper.py
+++ data/handwritten_helper.py
@@ -1,7 +1,7 @@
 from data_generation.data_generator import create_email_data
 
 if __name__ == "__main__":
-    email_prompt, email, summarized_points = create_email_data(gen_func=lambda x: "Write your own damn email!")
+    email_prompt, email, summarized_points = create_email_data([], gen_func=lambda x: "Write your own damn email!")
     # f = open("helper.txt", "w")
     # f.write("Prompt:\n" + email_prompt)
     # f.write(email)
